1831472d05980b425580494cf3da599523157d01
Gabriel Silva <gabsn.dev@gmail.com>
2023-02-24 23:19:21 -0300

    first commit

---
+++ src/test/java/io/github/com/gabrielsilvan/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.gabrielsilvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
